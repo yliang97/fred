e41eef64da8457a7557cb2479810b273fee57a59
Yijia Liang <yijial@princeton.edu>
2017-07-20 21:53:01 -0400

    updated index.js

--- index.js
+++ index.js
@@ -20,10 +20,14 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
+	if (req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+		console.log("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
 	}
-	res.send('Error, wrong token')
+	else {
+	res.send('Failed validation');
+	res.sendStatus(403);
+	}
 })
 
 // Spin up the server

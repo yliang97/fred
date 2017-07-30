b45466dce05bd291ca63733abce5960179b9fc70
Yijia Liang <yijial@princeton.edu>
2017-07-30 08:01:25 -0400

    using greeting to test v1

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/webhook', function (req, res) {
 function firstEntity(nlp, name, senderID) {
   sendTextMessage(senderID, JSON.stringify(nlp && nlp.entities));
   sendTextMessage(senderID, JSON.stringify(nlp && nlp.entities[name]));
-  sendTextMessage(senderID, JSON.stringify(nlp && nlp.entities && nlp.entities));
+  sendTextMessage(senderID, JSON.stringify(nlp && nlp.entities && nlp.entities[name]));
   return nlp && nlp.entities && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
 }
 

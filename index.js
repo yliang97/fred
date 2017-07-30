bde6ae01ff12aaf842e9b4401721fdf1c2bdbc1a
Yijia Liang <yijial@princeton.edu>
2017-07-29 22:27:29 -0400

    debugging wit.ai v1

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function handleMessage(senderID, message) {
 	if (intent && intent.confidence > 0.8) {
 		const location = firstEntity(message.nlp, 'location');
 		if (location && location.confidence > 0.8)
-			sendTextMessage(senderID, 'You are searching for' + location.value + ' ' + location.confidence);
+			sendTextMessage(senderID, 'You are searching for');
 			//sendTextMessage(senderID, 'The location of ' + location.value + 'is https://www.google.com/maps/place/' + location.value);
 		else
 			sendTextMessage(senderID, 'Failed to recognize command, try again');

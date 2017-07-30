efbde60226a23d2f2adb7f53c5f017e61fcce678
Yijia Liang <yijial@princeton.edu>
2017-07-30 07:23:11 -0400

    using greeting to test v1

--- index.js
+++ index.js
@@ -82,9 +82,9 @@ function firstEntity(nlp, name) {
 }
 
 function handleMessage(senderID, message) {
-	sendTextMessage(senderID, 'Check to see if this works ' + (JSON.stringify(message.nlp)));
+	//sendTextMessage(senderID, 'Check to see if this works ' + (JSON.stringify(message.nlp)));
     // check greeting is here and is confident
-    const greeting = firstEntity(message.nlp, 'greeting');
+    const greeting = firstEntity(message.nlp, 'greetings');
     if (greeting && greeting.confidence > 0.8) {
       sendTextMessage(senderID, 'Hi there!');
     } else { 

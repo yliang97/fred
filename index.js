367566c81c1439646bee1ca0ffab22c2d8bbdc8d
Yijia Liang <yijial@princeton.edu>
2017-08-01 22:20:34 -0400

    working with multiple js files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const jquery = require('jQuery')
 const app = express()
 const FB_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN
 var apiAiClient = require('apiai')(process.env.APIAI_SMALLTALK_TOKEN)
+var $ = require('jQuery');
 
 // booleans to determine which state the chatbot is in
 var GENERAL_QUESTIONS = false;
@@ -127,7 +127,7 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches a keyword
     // and send back the example. Otherwise, just echo the text we received.
     if (GENERAL_QUESTIONS == true) {
-      var response = jQuery.getScript('../bot_v1/princeton_general.js', function(){
+      var response = $.getScript('../bot_v1/princeton_general.js', function(){
         answerGeneric(senderID, message);
       });
       callSendAPI(response); 

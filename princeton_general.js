f338e7d2cbb014db983fe227a08c7533fae3de4d
Yijia Liang <yijial@princeton.edu>
2017-08-06 12:19:31 -0400

    refining princeton general info

--- princeton_general.js
+++ princeton_general.js
@@ -10,7 +10,8 @@ function firstEntity(nlp, name) {
 exports.interpretGeneric = function(senderID, message) {
 	const social = firstEntity(message.nlp, 'princeton_general_social');
 	const admission = firstEntity(message.nlp, 'princeton_general_admission');
-	const classes = firstEntity(message.nlp, 'princeton_general_classes')
+	const classes = firstEntity(message.nlp, 'princeton_general_classes');
+	const difficulty = firstEntity(message.nlp, 'princeton_general_difficulty');
 	const general = firstEntity(message.nlp, 'princeton_general');
 	console.log(social);
 	console.log(admission);
@@ -21,7 +22,14 @@ exports.interpretGeneric = function(senderID, message) {
 		if (admission && admission.confidence > 0.8)
 			return sendTextMessage(senderID, "For admission information, please see " + admissionLink);
 		if (classes && classes.confidence > 0.8)
-			return sendTextMessage(senderID, "If you are interested in social life on campus, please select the social life tab on the menu.");
+			return sendTextMessage(senderID, "If you are interested in how classes are, please select the classes tab on the menu.");
+		if (difficulty && difficulty.confidence > 0.8) {
+			if (difficulty.value == 'easy' || difficulty.value == 'simple')
+				sendTextMessage(senderID, "Princeton will be harder than you expect! Don't underestimate the difficulty of classes");
+			else if (difficulty.value == 'hard' || difficulty.value == 'difficult' || difficulty.value = 'strenuous')
+				sendTextMessage(senderID, "Yes, Princeton is hard. Yet don't be worred! Check the classes tab for more information about how to prepare.")
+		}
+
 	}
 
 
